f9719952dd02165f15d3bcf2797ae7dec46defcd
titanslayer2746 <yatin.2022ug1074@iiitranchi.ac.in>
2025-09-01 15:30:02 +0530

    Middleware Setup done

---
+++ backend/middleware/authmiddleware.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    if (!req.session.user) {
+        return res.status(401).json({ message: "Unauthorized. Please log in first." });
+    }
+    next();
+};
+
+
+ 
