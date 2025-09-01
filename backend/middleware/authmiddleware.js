ef93f33ea7f46ad203389890af696f7e848e7b25
titanslayer2746 <yatin.2022ug1074@iiitranchi.ac.in>
2025-09-01 23:42:54 +0530

    Refactor auth middleware for improved readability and consistency

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,9 +1,8 @@
 module.exports = (req, res, next) => {
-    if (!req.session.user) {
-        return res.status(401).json({ message: "Unauthorized. Please log in first." });
-    }
-    next();
+  if (!req.session.user) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized. Please log in first." });
+  }
+  next();
 };
-
-
- 
