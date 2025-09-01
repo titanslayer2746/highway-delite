1ea87120541df6edb053d62e3998924dde53732d
titanslayer2746 <yatin.2022ug1074@iiitranchi.ac.in>
2025-09-01 23:07:38 +0530

    Update User model schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,16 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    otp: { type: String }, // OTP for verification
-    otpExpiry: { type: Date }, // Expiry time for OTP
-    isVerified: { type: Boolean, default: false } // Email verification status
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  dob: { type: Date, required: true }, // Date of birth
+  otp: { type: String }, // OTP for verification
+  otpExpiry: { type: Date }, // Expiry time for OTP
+  isVerified: { type: Boolean, default: false }, // Email verification status
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
-
- 
